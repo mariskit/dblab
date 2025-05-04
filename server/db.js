dedbce99e45a0d25d751e8282c1c494f2de2baa7
mariskite <dahoyoss@gmail.com>
2025-05-04 15:41:34 -0500

    grid mas mapa

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+//db para vercel
 const sql = require('mssql');
 
 const config = {
@@ -30,3 +31,41 @@ const poolPromise = new sql.ConnectionPool(config)
   });
 
 module.exports = { poolPromise, sql };
+
+/*
+// db.js para local
+const sql = require('mssql');
+
+const config = {
+  server: 'covid-dashboard-sql-server.database.windows.net',
+  authentication: {
+    type: 'default',
+    options: {
+      userName: 'covidadmin',
+      password: 'P4ssw0rd!Seguro'
+    }
+  },
+  options: {
+    database: 'covid-dashboard-db',
+    encrypt: true,
+    trustServerCertificate: false
+  }
+};
+
+const pool = new sql.ConnectionPool(config);
+const poolPromise = pool.connect()
+  .then(() => {
+    console.log('Conexión a SQL Server exitosa');
+    return pool;
+  })
+  .catch(err => {
+    console.error('Error de conexión a la base de datos:', err);
+    process.exit(1);
+  });
+
+// Exporta tanto poolPromise como sql
+module.exports = {
+  poolPromise,
+  sql
+};
+*/ 
