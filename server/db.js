b53045a51a32cbbbe58c9ef342a29d1ba2e71b9a
mariskite <dahoyoss@gmail.com>
2025-05-04 16:42:14 -0500

    conexion a azure, proto final 1

--- server/db.js
+++ server/db.js
@@ -1,4 +1,38 @@
+//db para verel
+const sql = require('mssql');
 
+const config = {
+  server: process.env.DB_SERVER,
+  authentication: {
+    type: 'default',
+    options: {
+      userName: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    }
+  },
+  options: {
+    database: process.env.DB_NAME,
+    encrypt: true, // SSL obligatorio en Azure
+    trustServerCertificate: false, // Para producción
+    requestTimeout: 30000, // 30 segundos
+    connectionTimeout: 30000
+  }
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Conexión exitosa a SQL Server');
+    return pool;
+  })
+  .catch(err => {
+    console.error('Error de conexión:', err);
+    throw err;
+  });
+
+module.exports = { poolPromise, sql };
+
+/*
 // db.js para local
 const sql = require('mssql');
 
@@ -34,3 +68,4 @@ module.exports = {
   poolPromise,
   sql
 };
+*/ 
